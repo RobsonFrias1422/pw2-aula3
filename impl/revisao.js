85a7ba9c7bd1691c6ea5c62d5935b721e9e8ceae
RobsonFrias1422 <robsonfrias@gmail.com>
2019-08-14 20:28:46 -0300

    revisao -ts

---
+++ impl/revisao.js
@@ -0,0 +1,11 @@
+var nota1 = 6;
+var nota2 = 7;
+var nota3 = 5;
+var media = (nota1 + nota2 + nota3) / 3;
+console.log("A nota das medias é" + media);
+if (media >= 5) {
+    console.log("Média Maior que 5");
+}
+else {
+    console.log("Média Menor que 5");
+}
---
+++ revisao.ts
@@ -0,0 +1,11 @@
+let nota1: number =6
+let nota2: number =7
+let nota3: number =5
+let media = (nota1 + nota2 + nota3) / 3
+console.log("A nota das medias é" + media)
+if (media>=5){
+    console.log("Média Maior que 5")
+}
+ else {
+    console.log("Média Menor que 5")
+}
